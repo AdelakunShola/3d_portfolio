47378e50844e7974e2891995700276eff8f09b90
Adelakun Shola <officialadelakun@gmail.com>
2023-10-07 12:16:19 -1100

    .

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -51,7 +51,7 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop='demand'
       shadows
-      dpr={[1, 2]}
+      dpr={isMobile ? [1] : [1, 2]} // Adjust device pixel ratio based on device
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
