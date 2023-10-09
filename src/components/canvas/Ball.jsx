5e05ea5dfb597676c122970a1f35d487bf473115
Adelakun Shola <officialadelakun@gmail.com>
2023-10-09 08:12:44 -1100

    .

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,27 +1,29 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Decal, Float, Preload, useTexture } from "@react-three/drei";
+import {
+  Decal,
+  Float,
+  useTexture,
+} from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+    <Float>
+      <ambientLight intensity={100} />
+      
+      <mesh   scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
+          color='#fff8eb'
         />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
+         
           map={decal}
           flatShading
         />
@@ -30,15 +32,17 @@ const Ball = (props) => {
   );
 };
 
-const BallCanvas = ({ icons }) => {
+const BallCanvas = ({ icon }) => {
   return (
-    <Canvas>
+    <Canvas
+     
+    >
       <Suspense fallback={<CanvasLoader />}>
-        {icons.map((icon, index) => (
-          <Ball key={index} imgUrl={icon} />
-        ))}
+   
+        <Ball imgUrl={icon} />
       </Suspense>
-      <Preload all />
+
+     
     </Canvas>
   );
 };
